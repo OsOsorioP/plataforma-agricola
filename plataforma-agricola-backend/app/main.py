a6217cf203484c92b8b08066c104b80eceda4139
workspace014-admin <oscar.osorio1884@gmail.com>
2025-09-16 13:08:30 -0500

    feat(backend): integración y routers en la aplicación main

--- plataforma-agricola-backend/app/main.py
+++ plataforma-agricola-backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from app.api import users, parcels
 
 app = FastAPI(
     title="Plataforma Multiagrente para Agricultura Sostenible",
     description="API para gestionar la interacción entre agricultores y agentes de IA.",
     version="0.1.0"
 )
+
+app.include_router(router=users.router, prefix="/users", tags=["Users"])
+app.include_router(router=parcels.router, prefix="/parcels", tags=["Parcels"])
 
 @app.get("/")
 def read_root():
