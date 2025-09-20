3c6453c09f53bff1f1bd3f41badcd89c109facec
workspace014-admin <oscar.osorio1884@gmail.com>
2025-09-20 11:31:48 -0500

    feat(db): crear modelo de base de datos para los mensajes

--- plataforma-agricola-backend/app/db_models.py
+++ plataforma-agricola-backend/app/db_models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Boolean, Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from app.database import Base
 
 from datetime import datetime
@@ -12,7 +13,7 @@ class User(Base):
     full_name = Column(String(50), nullable=False)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
-    created_at = Column(DateTime(), default=datetime.now())
+    created_at = Column(DateTime(timezone=True), default=datetime.now())
 
     parcels = relationship("Parcel", back_populates="owner")
 
@@ -26,3 +27,14 @@ class Parcel(Base):
     owner_id = Column(Integer, ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="parcels")
+    
+class ChatMessage(Base):
+    __tablename__ = 'chat_message'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    # El session_id nos va a permitir agrupar conversaciones, se implementará a futuro.
+    # session_id = Column(String, index=True)
+    sender_type = Column(String, nullable=False) # 'user' o 'ai'
+    message = Column(String, nullable=False)
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
