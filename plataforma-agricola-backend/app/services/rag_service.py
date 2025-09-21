bc0c968f4bb3d7f95aaeb42079a24eb41d1fa9ea
workspace014-admin <oscar.osorio1884@gmail.com>
2025-09-21 13:59:27 -0500

    fix(service): agregar un embedding local, para evitar problemas con limit de gemini

--- plataforma-agricola-backend/app/services/rag_service.py
+++ plataforma-agricola-backend/app/services/rag_service.py
@@ -3,6 +3,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 from app.core.config import GOOGLE_API_KEY
 
@@ -33,7 +34,7 @@ def build_vectorstore():
     
     # Crear embeddings y alamacernar en ChromaDB
     # Los embeddings se convierten de texto a vectores numéricos
-    embedding_function = GoogleGenerativeAIEmbeddings(model='models/embedding-001', google_api_key=GOOGLE_API_KEY)
+    embedding_function = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
     
     vectorstore = Chroma.from_documents(
         documents=splits,
@@ -49,8 +50,12 @@ vectorstore = build_vectorstore()
 
 def get_retriever():
     """
-    Obtiene un 'retriever' que se puede buscar en la base de datos vectorial.
+    Obtiene un 'retriever' que puede buscar en la base de datos vectorial.
     """
     if vectorstore is None:
-        return None
+        # Si no hay documentos, se crea un retriever "tonto" que no devuelve nada.
+        class EmptyRetriever:
+            def get_relevant_documents(self, query):
+                return []
+        return EmptyRetriever()
     return vectorstore.as_retriever(search_kwargs={'k': 3}) # Aquí el k=3 significa que buscará los 3 fragmentos más relevantes
