699dfe83241f92385bfed93f7fdd012c7fb2fb99
workspace014-admin <oscar.osorio1884@gmail.com>
2025-11-19 15:58:08 -0500

    feat(navigation): actualizar los enlaces de navegación y mejorar la funcionalidad de la pantalla de inicio

--- plataforma-agricola-frontend/app/_layout.tsx
+++ plataforma-agricola-frontend/app/_layout.tsx
@@ -1,43 +1,34 @@
 // app/_layout.tsx
 import { Stack } from 'expo-router';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { ActivityIndicator, View, StyleSheet} from 'react-native';
-import { useAuth, AuthProvider } from '@/context/AuthContext';
-
-function InitialLoadingScreen() {
-  const { isLoading } = useAuth();
-  if (isLoading) {
-    return (
-      <View style={styles.centered}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
-    );
-  }
-  return null;
-}
-
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, useColorScheme } from 'react-native';
+import { AuthProvider } from '@/context/AuthContext';
 
 export default function RootLayout() {
-  return (
-    <SafeAreaProvider style={styles.container}>
+  let colorScheme = useColorScheme()
 
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: colorScheme === 'light' ? "#333" : "#fff" }}>
+      <SafeAreaProvider style={styles.container}>
         <AuthProvider>
-          <InitialLoadingScreen />
           <Stack screenOptions={{
             headerShown: false,
           }}>
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="index" />
+            <Stack.Screen name="(auth)" />
+            <Stack.Screen name="(tabs)" />
             <Stack.Screen name="+not-found" />
           </Stack>
         </AuthProvider>
-    </SafeAreaProvider>
+      </SafeAreaProvider>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
   },
   centered: {
     flex: 1,
--- plataforma-agricola-frontend/app/index.tsx
+++ plataforma-agricola-frontend/app/index.tsx
@@ -1,22 +1,43 @@
-import { Text } from "@react-navigation/elements";
-import { Link } from "expo-router";
-import { View, StyleSheet } from "react-native";
+import { StatusBar, StyleSheet, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { colors } from '@/constants/theme'
 
-export default function HomeScreen() {
+import Animated, { FadeInDown } from 'react-native-reanimated'
+import { useRouter } from 'expo-router'
+import Typo from '@/components/ui/Typo'
+
+const SplashScreen = () => {
+    const router = useRouter()
+
+    useEffect(() => {
+        setTimeout(() => { router.replace('/(auth)/welcome') }, 2000);
+    }, [router])
+
     return (
         <View style={styles.container}>
-            <Text>
-                Hola
-            </Text>
-            <Link href="/(auth)/signin/index">Navigate to nested route</Link>
+            <StatusBar barStyle={'light-content'} backgroundColor={colors.background} />
+            <Animated.Image
+                source={require('@/assets/images/logo.png')}
+                entering={FadeInDown.duration(700).springify()}
+                style={styles.logo}
+                resizeMode={"contain"}
+            />
+            <Typo>Agrosmi</Typo>
         </View>
     )
 }
 
+export default SplashScreen
+
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center',
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: colors.background
+    },
+    logo: {
+        height: '23%',
+        aspectRatio: 1,
     }
 })
