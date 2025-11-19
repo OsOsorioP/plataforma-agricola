849e23923bc3c51fec5ad7a9adc71b81495c9479
workspace014-admin <oscar.osorio1884@gmail.com>
2025-11-19 16:17:47 -0500

    feat(chat_component): agregar componente de chat para los items de los mensajes

---
+++ plataforma-agricola-frontend/components/chat/MessageItem.tsx
@@ -0,0 +1,78 @@
+import { StyleSheet, View } from 'react-native'
+import React from 'react'
+import { MessageProps } from '@/types/components'
+import { useAuth } from '@/context/AuthContext'
+import { scale, verticalScale } from '@/utils/styling'
+import { colors, spacingX, spacingY } from '@/constants/theme'
+import Typo from '../ui/Typo'
+import { Image } from 'expo-image'
+import { getFileFromBase64 } from '@/utils/base64'
+
+export default function MessageItem({
+    item
+}: { item: MessageProps }) {
+    const { user: currentUser } = useAuth()
+    const isMe = item.isMe
+
+    return (
+        <View
+            style={[
+                styles.messageContainer,
+                isMe ? styles.myMessage : styles.theirMessage
+            ]}
+        >
+            {
+                !isMe && (
+                    <></>
+                )
+            }
+            <View
+                style={[
+                    styles.messageBubble,
+                    isMe ? styles.myBubble : styles.theirBubble
+                ]}
+            >
+                {!isMe && (
+                    <Typo color={colors.neutral400} fontWeight={"600"} size={13}>
+                        {item.sender}
+                    </Typo>
+                )}
+                {item.attachement && (<Image source={{ uri: `data:image/jpeg;base64,${item.attachement}` }} contentFit='cover' style={styles.attachment} transition={100} />)}
+                {item.content && (<Typo size={15}>{item.content}</Typo>)}
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    messageContainer: {
+        flexDirection: "row",
+        gap: spacingX._7,
+        maxWidth: "90%",
+    },
+    myMessage: {
+        alignItems: "flex-end"
+    },
+    theirMessage: {
+        alignItems: "flex-start"
+    },
+    messageAvatar: {
+        alignSelf: "flex-end",
+    },
+    attachment: {
+        height: verticalScale(180),
+        width: verticalScale(180),
+        borderRadius: scale(10),
+    },
+    messageBubble: {
+        padding: spacingX._10,
+        borderRadius: scale(15),
+        gap: spacingY._5,
+    },
+    myBubble: {
+        backgroundColor: colors.white,
+    },
+    theirBubble: {
+        backgroundColor: colors.green
+    },
+})
