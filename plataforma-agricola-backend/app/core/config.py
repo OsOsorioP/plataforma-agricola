330507ae13e053cee732dfb1f667341cbd14edfa
workspace014-admin <oscar.osorio1884@gmail.com>
2025-11-19 15:06:20 -0500

    feat(config): agregar claves de acceso para DATOS_GOV en la configuración

--- plataforma-agricola-backend/app/core/config.py
+++ plataforma-agricola-backend/app/core/config.py
@@ -10,5 +10,9 @@ SECRET_KEY:str = os.getenv("SECRET_KEY")
 ALGORITHM:str = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES:int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
+DATOS_GOV_API_KEY:str = os.getenv("DATOS_GOV_API_KEY")
+DATOS_GOV_USER:str = os.getenv("DATOS_GOV_USER")
+DATOS_GOV_PASSWORD:str = os.getenv("DATOS_GOV_PASSWORD")
+
 if not GOOGLE_API_KEY:
     raise ValueError("No se encontró la GOOGLE_API_KEY en el entorno. Asegúrate de tener un archivo .env válido.")
