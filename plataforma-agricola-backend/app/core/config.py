1ca99ebcabcbf1bd4b2c97509792251869d14399
workspace014-admin <oscar.osorio1884@gmail.com>
2025-11-21 15:08:02 -0500

    update(core): actualizar nuevas variables de entorno, y definir modelos para cada agente que se usarán

--- plataforma-agricola-backend/app/core/config.py
+++ plataforma-agricola-backend/app/core/config.py
@@ -3,16 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_API_KEY:str = os.getenv("GOOGLE_API_KEY")
-OPENWEATHER_API_KEY:str = os.getenv("OPENWEATHER_API_KEY")
+GOOGLE_API_KEY: str = os.getenv("GOOGLE_API_KEY")
+GROQ_API_KEY: str = os.getenv("GROQ_API_KEY")
 
-SECRET_KEY:str = os.getenv("SECRET_KEY")
-ALGORITHM:str = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES:int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+OPENWEATHER_API_KEY: str = os.getenv("OPENWEATHER_API_KEY")
 
-DATOS_GOV_API_KEY:str = os.getenv("DATOS_GOV_API_KEY")
-DATOS_GOV_USER:str = os.getenv("DATOS_GOV_USER")
-DATOS_GOV_PASSWORD:str = os.getenv("DATOS_GOV_PASSWORD")
+SECRET_KEY: str = os.getenv("SECRET_KEY")
+ALGORITHM: str = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+DATOS_GOV_API_KEY: str = os.getenv("DATOS_GOV_API_KEY")
+DATOS_GOV_USER: str = os.getenv("DATOS_GOV_USER")
+DATOS_GOV_PASSWORD: str = os.getenv("DATOS_GOV_PASSWORD")
 
 if not GOOGLE_API_KEY:
-    raise ValueError("No se encontró la GOOGLE_API_KEY en el entorno. Asegúrate de tener un archivo .env válido.")
+    raise ValueError(
+        "No se encontró la GOOGLE_API_KEY en el entorno. Asegúrate de tener un archivo .env válido.")
---
+++ plataforma-agricola-backend/app/core/llm_provider.py
@@ -0,0 +1,61 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
+
+from app.core.config import (
+    GOOGLE_API_KEY,
+    GROQ_API_KEY,
+)
+
+llm_supervisor = ChatGroq(
+    model_name="llama3-70b-8192",
+    temperature=0,
+    groq_api_key=GROQ_API_KEY
+)
+
+# llm_water = ChatGroq(
+#    model="llama-3.3-70b-versatile",
+#    api_key=GROQ_API_KEY,
+#    temperature=0
+# )
+
+llm_water = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
+    google_api_key=GOOGLE_API_KEY
+)
+
+llm_risk = ChatGroq(
+    model_name="llama3-8b-8192",
+    temperature=0,
+    groq_api_key=GROQ_API_KEY
+)
+
+llm_supply_chain = ChatGroq(
+    model_name="llama3-8b-8192",
+    temperature=0,
+    groq_api_key=GROQ_API_KEY
+)
+
+llm_vision = ChatGoogleGenerativeAI(
+    model="gemini-1.5-pro-latest",
+    temperature=0,
+    google_api_key=GOOGLE_API_KEY
+)
+
+llm_production = ChatGroq(
+    model_name="mixtral-8x7b-32768",
+    temperature=0,
+    groq_api_key=GROQ_API_KEY
+)
+
+llm_sustainability = ChatGroq(
+    model="llama3-8b-8192",
+    temperature=0,
+    google_api_key=GOOGLE_API_KEY
+)
+
+llm_kpi = ChatGroq(
+    model_name="gemma-7b-it",
+    temperature=0,
+    groq_api_key=GROQ_API_KEY
+)
